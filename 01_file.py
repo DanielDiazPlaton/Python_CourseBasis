7d85d26f14f0cdc5ad378e3439e3f3375f866abd
Daniel Diaz <daniel_dp96@hotmail.com>
2023-04-12 22:15:17 -0600

    files

---
+++ 01_file.py
@@ -0,0 +1,34 @@
+print('Operations')
+
+print(5 + 6)
+print(5 - 6)
+print(5 * 6)
+print(5 / 6)
+print(5 // 6)    # Ignore the decimal point and it doesn't redounded the decimal point
+print(5 % 6)     # Module of division
+print(5 ** 2)
+print(5 ** 3)
+
+# Variables
+
+print('\n')
+print('Variables')
+print()
+result = 10 + 5
+print(result)
+result2 = result / 2
+print()
+print(result2)
+
+test_variable = 'test'
+print(test_variable)
+
+print()
+num1 = 10
+print(num1)
+num1 *= 10
+print(num1)
+num1 /= 10
+print(num1)
+num1 -= 10
+print(num1)
